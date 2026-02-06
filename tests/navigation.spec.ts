a52f34cb5d236d36f9640fae4c3b99b5470f80a6
AlexG3178 <alexg3178@gmail.com>
2026-02-06 17:06:50 +0100

    adding navigation test

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { BasePage } from './pages/BasePage';
+
+test.describe('Header navigation – webservis.net', () => {
+  test('Projects link routes correctly @smoke', async ({ page }) => {
+    const basePage = new BasePage(page);
+
+    await page.goto('/');
+    await basePage.header.goTo('Projects');
+
+    await expect(page).toHaveURL(/\/projects\/?$/);
+  });
+
+  test('Services link routes correctly @smoke', async ({ page }) => {
+    const basePage = new BasePage(page);
+
+    await page.goto('/');
+    await basePage.header.goTo('Services');
+
+    await expect(page).toHaveURL(/\/services\/?$/);
+  });
+
+  test('Contacts link routes correctly @smoke', async ({ page }) => {
+    const basePage = new BasePage(page);
+
+    await page.goto('/');
+    await basePage.header.goTo('Contacts');
+
+    await expect(page).toHaveURL(/\/contacts\/?$/);
+  });
+});
